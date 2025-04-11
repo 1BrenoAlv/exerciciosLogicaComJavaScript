eab90ae6730ed10fe0edafc7d377981d54d9fbc2
1Brenoalv <biomassa141@gmail.com>
2025-04-11 00:47:37 -0300

    dia 11/04

--- faccat/js/ex08.js
+++ faccat/js/ex08.js
@@ -1,3 +1,12 @@
 function exercicio8(){
-    alert("")
+    alert("Apuração de Votos")
+    let votos = parseInt(prompt("Digite o total de votos: "))
+    let validos = parseInt(prompt("Digite o total de votos validos: "))
+    let brancos = parseInt(prompt("Digite o total de votos em branco: "))
+    let nulos = parseInt(prompt("Digite o total de votos nulo: "))
+    let apuracaoV = validos*votos/100
+    let apuracaoB  = brancos*votos/100
+    let apuracaoN  = nulos*votos/100
+    alert("O total de votos é: "+votos)
+    alert("Válidos: "+apuracaoV+"% "+"Branco: "+apuracaoB+"% "+"Nulos: "+apuracaoN+"%")
 }
