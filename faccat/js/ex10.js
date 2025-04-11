b4aa8eedadf7dc56c7944acac479d6ea6b895399
1Brenoalv <biomassa141@gmail.com>
2025-04-11 17:25:26 -0300

    att

---
+++ faccat/js/ex10.js
@@ -0,0 +1,7 @@
+function exercicio10(){
+    alert("Calcular custo final")
+    let custoFabrica = parseInt(prompt("Digite o custo de fabricação: "))
+    let  distruidora = custoFabrica*28/100
+    let  impostos = custoFabrica*45/100
+    alert(`O custo final é R$${custoFabricaustoFabrica+distruidora+impostos}`)
+}
